e8393e1378e1585a005666af8ae32cfe948ba56b
EndlessConundrums <wilcoxgonzalezn27@bcdschool.org>
2025-09-07 19:04:18 +0000

    Finished encryptor

--- rot13.py
+++ rot13.py
@@ -1,17 +1,22 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-rot13bet = "nopqrstuvwxyzabcdefghijklm"
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+rot13bet = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+extrabet = " .,?!:;<>'\""
 def encryptor(plaintext):
     cyphertext = []
     for i in plaintext:
         for j in range(len(alphabet)):
             if i == alphabet[j]:
                 cyphertext.append(rot13bet[j])
-                print("letter appended")
+        for k in range(len(extrabet)):
+            if i == extrabet[k]:
+                cyphertext.append(extrabet[k])
     return cyphertext
 
 
 sample = "Hello world!"
 temp = encryptor(sample)
 cypherSample = "".join(temp)
-print(temp)
 print(cypherSample)
+temp = encryptor(cypherSample)
+sample = "".join(temp)
+print(sample)
